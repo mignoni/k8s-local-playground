fcbce6cdcc88bccd505b99ef3cff6428f255837f
Bruno Mignoni <mignoni@gmail.com>
2022-12-04 11:06:17 -0300

    Update create-ingress-in-cluster.sh

--- kind/create-ingress-in-cluster.sh
+++ kind/create-ingress-in-cluster.sh
@@ -6,8 +6,8 @@ echo '------- Creating Ingress Resources!'
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 echo '------- '
 
-#echo '------- ------- ------- ------- ------- '
-#echo '------- Waiting the creation of Ingress resources '
-#kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s
-#echo '------- '
+echo '------- ------- ------- ------- ------- '
+echo '------- Waiting the creation of Ingress resources '
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s
+echo '------- '
 
